a213ca6bcf37b7f280ce91aa43219dca18754c80
festus-Sulumeti <126145210+festus-sulumeti@users.noreply.github.com>
2026-07-12 19:12:02 +0300

    fiix(Navbar & Footer):  Update status
    Removed Navbar and Footer components from the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Footer from './components/Footer'
+
 
 function App() {
   return (
     <>
-      <Navbar />
+      
       <Routes>
         <Route path="/" element={<Hero />} />
       </Routes>
-      <Footer />
     </>
   )
 }
