f8e852d8a7f6892d01a74fe53f110856b0cdc808
festus-sulumeti <sulumetifestus@outlook.com>
2026-07-19 17:24:46 +0300

    feat: contact us route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import Hero from './components/Hero'
 import Whoweare from './components/Navigation/Whoweare';
 import OurModel from './components/Navigation/OurModel';
+import ContactUs from './components/Navigation/ContactUs';
+
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
         <Route path="/" element={<Hero />} />
         <Route path="/who-we-are" element={<Whoweare />} />
         <Route path="/our-model" element={<OurModel />} />
+        <Route path="/contact-us" element={<ContactUs />} />
       </Routes>
     </>
   )
