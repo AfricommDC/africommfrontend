a3f7200fa88b521d089bcbeffa7a183615cb74ca
festus-sulumeti <sulumetifestus@outlook.com>
2026-07-19 16:26:58 +0300

    :feat our model route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Hero from './components/Hero'
 import Whoweare from './components/Navigation/Whoweare';
+import OurModel from './components/Navigation/OurModel';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/who-we-are" element={<Whoweare />} />
+        <Route path="/our-model" element={<OurModel />} />
       </Routes>
     </>
   )
